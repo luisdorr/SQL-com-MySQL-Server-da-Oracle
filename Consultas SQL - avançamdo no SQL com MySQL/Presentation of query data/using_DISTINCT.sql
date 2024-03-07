db1c43f51f318533827dd3ce3d3452faec9340f7
Luis Dorr <Luisdorr@outlook.com>
2024-03-07 16:56:12 -0300

    BKP

--- Consultas SQL - avançamdo no SQL com MySQL/Presentation of query data/using_DISTINCT.sql
+++
@@ -1,9 +0,0 @@
-SELECT EMBALAGEM, TAMANHO FROM tabela_de_produtos;
-
-SELECT DISTINCT EMBALAGEM, TAMANHO FROM tabela_de_produtos;
-
-SELECT DISTINCT EMBALAGEM, TAMANHO FROM tabela_de_produtos
-WHERE SABOR = 'laranja';
-
-SELECT DISTINCT tabela_de_clientes.BAIRRO FROM tabela_de_clientes
-WHERE CIDADE = 'Rio de janeiro';
